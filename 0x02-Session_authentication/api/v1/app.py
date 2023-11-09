cf5eaa6c3b6c54073793758b81bf38e864f39e67
mideactive <jenyoolumide@yahoo.com>
2023-11-09 09:51:54 +0000

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,31 +5,29 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
-
+from flask_cors import CORS
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
+AUTH_TYPE = getenv("AUTH_TYPE")
+
 if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 elif AUTH_TYPE == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 elif AUTH_TYPE == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
-
 
 @app.before_request
 def bef_req():
@@ -47,15 +45,14 @@ def bef_req():
             '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            if isinstance(auth, BasicAuth):
-                cookie = None
-            else:
+            if not isinstance(auth, BasicAuth):
                 cookie = auth.session_cookie(request)
+            else:
+                cookie = None
             if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -63,20 +60,17 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Request unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Request unauthorized handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
